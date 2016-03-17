c2b3ee696096b7fea34f3e41fad02309192a85ca
Tomasz Klim <github@tomaszklim.pl>
2016-03-17 23:18:37 +0100

    changed extension base path

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@
 
 
 
-base=/opt/sf-samba-server/templates/$OSVER
+base=/opt/farm/ext/samba-server/templates/$OSVER
 
 if [ ! -f $base/smb.conf.tpl ]; then
 	echo "skipping samba setup, unsupported operating system version"
 	exit 1
 fi
 
-bash /opt/farm/scripts/setup/role.sh samba
-bash /opt/farm/scripts/setup/role.sh sf-php
+/opt/farm/scripts/setup/role.sh samba
+/opt/farm/scripts/setup/role.sh sf-php
 
 /etc/init.d/samba stop
 update-rc.d -f samba remove
@@ -25,4 +25,4 @@ if [ ! -f /etc/local/.config/samba.php ]; then
 	install_copy $base/samba.php.tpl /etc/local/.config/samba.php
 fi
 
-ln -sf /opt/sf-samba-server/samba-refresh.php /usr/local/bin/samba-refresh
+ln -sf /opt/farm/ext/samba-server/samba-refresh.php /usr/local/bin/samba-refresh
